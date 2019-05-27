f102a1be7be4467419d8a5556a8991b1b821bb42
Stewie <petersloan1984@gmail.com>
2019-05-27 16:46:24 +1200

    conditional routing

--- assets/js/redirect.js
+++ assets/js/redirect.js
@@ -10,7 +10,14 @@ function submitForm() {
   data = [name, email, company, budget];
   console.log(data);
 
-  redirectURL = "https://stewiethepro.github.io/form-test/booking-page.html?soskip=1&name=" + name + "&email=" + email + "&company=" + company + "&budget=" + budget
-  console.log(redirectURL);
-  window.open(redirectURL, '_blank');
+  if (demoForm.budget.value != 'Under $1000') {
+    redirectURL = "https://stewiethepro.github.io/form-test/booking-page.html?soskip=1&name=" + name + "&email=" + email + "&company=" + company + "&budget=" + budget
+    console.log(redirectURL);
+    window.open(redirectURL, '_blank');
+  }
+  else {
+    redirectURL = "https://stewiethepro.github.io/form-test/discover-our-product.html?soskip=1&name=" + name + "&email=" + email + "&company=" + company + "&budget=" + budget
+    console.log(redirectURL);
+    window.open(redirectURL, '_blank');
+  }
 };
