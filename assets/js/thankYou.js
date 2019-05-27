3bb970283f4685d77c9f495b16cf718878e4e68e
Stewie <petersloan1984@gmail.com>
2019-05-27 17:25:51 +1200

    thank you page added

---
+++ assets/js/thankYou.js
@@ -0,0 +1,25 @@
+function thankYou(){
+
+  var url_string = window.location.href;
+  var decoded = decodeURI(url_string);
+  var url = new URL(decoded);
+  var name = url.searchParams.get("name");
+
+  var nameString = name.split(" ");
+  var firstName = nameString[0];
+
+
+  console.log(nameString);
+  console.log(firstName);
+
+  const thankYouMessage = document.querySelector('#thank-you-message-heading');
+
+  thankYouMessage.innerHTML =
+  `
+  <h1 class="display-4 text-center mb-3">
+    Thank you, ${firstName}.
+  </h1>
+  `
+};
+
+thankYou();
