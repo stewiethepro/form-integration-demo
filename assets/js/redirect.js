c0290d0e6cc97c8720371f8a87ae5d5e5cbad2b6
Stewie <petersloan1984@gmail.com>
2019-06-14 13:19:25 +1200

    v1.3 Changes to form submit

--- assets/js/redirect.js
+++ assets/js/redirect.js
@@ -1,6 +1,8 @@
-function submitForm() {
 
-  var demoForm = document.getElementById("demo-form");
+const demoForm = document.getElementById("demo-form");
+
+demoForm.addEventListener("submit", function(e) {
+e.preventDefault();
 
   name = encodeURIComponent(demoForm.name.value);
   email = encodeURIComponent(demoForm.email.value);
@@ -12,12 +14,12 @@ function submitForm() {
 
   if (demoForm.budget.value != 'Under $1000') {
     redirectURL = "https://stewiethepro.github.io/form-test/booking-page.html?soskip=1&name=" + name + "&email=" + email + "&company=" + company + "&budget=" + budget
-    console.log(redirectURL);
-    window.open(redirectURL, '_blank');
+    console.log(redirectURL)
+    window.open(redirectURL, '_blank')
   }
   else {
     redirectURL = "https://stewiethepro.github.io/form-test/discover-our-product.html?soskip=1&name=" + name + "&email=" + email + "&company=" + company + "&budget=" + budget
-    console.log(redirectURL);
-    window.open(redirectURL, '_blank');
-  }
-};
+    console.log(redirectURL)
+    window.open(redirectURL, '_blank')
+  };
+});
